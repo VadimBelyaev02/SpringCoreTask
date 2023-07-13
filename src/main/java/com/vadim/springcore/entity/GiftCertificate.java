d6d3ac085323eb9e89a4cac7e885cdfebf44f500
VadimBelyaev02 <vadimbelaev002@gmail.com>
2023-07-13 15:55:59 +0300

    Added entities for existed tables

---
+++ src/main/java/com/vadim/springcore/entity/GiftCertificate.java
@@ -0,0 +1,39 @@
+package com.vadim.springcore.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "gift_certificates")
+public class GiftCertificate {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    private UUID id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "price")
+    private BigDecimal price;
+
+    @Column(name = "duration")
+    private Integer duration;
+
+    @Column(name = "create_date")
+    private Instant createDate;
+
+    @Column(name = "last_update_date")
+    private Instant lastUpdateDate;
+}
---
+++ src/main/java/com/vadim/springcore/entity/Tag.java
@@ -0,0 +1,28 @@
+package com.vadim.springcore.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tags")
+public class Tag {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    private UUID id;
+
+    @Column(name = "name")
+    private String name;
+}
