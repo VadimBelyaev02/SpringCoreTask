3810bad02ecd43a1c56a9a7ec33a7a417dbe6995
VadimBelyaev02 <vadimbelaev002@gmail.com>
2023-07-13 22:09:52 +0300

    Added TagResponseDto implementation

--- src/main/java/com/vadim/springcore/dto/response/TagResponseDto.java
+++ src/main/java/com/vadim/springcore/dto/response/TagResponseDto.java
@@ -1,4 +1,16 @@
 package com.vadim.springcore.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class TagResponseDto {
+
+    private String name;
 }
