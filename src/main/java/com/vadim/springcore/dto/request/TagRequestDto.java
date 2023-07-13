95a56d7efc60ee5f0fc23c380b835a53ea792e3c
VadimBelyaev02 <vadimbelaev002@gmail.com>
2023-07-13 22:11:19 +0300

    Added implementation for TagRequestDto

--- src/main/java/com/vadim/springcore/dto/request/TagRequestDto.java
+++ src/main/java/com/vadim/springcore/dto/request/TagRequestDto.java
@@ -1,4 +1,13 @@
 package com.vadim.springcore.dto.request;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
 public class TagRequestDto {
+
+    private String name;
 }
