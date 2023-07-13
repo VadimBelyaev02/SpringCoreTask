b70364caefdf6358344405a7a030aac7cd8ed678
VadimBelyaev02 <vadimbelaev002@gmail.com>
2023-07-13 22:38:00 +0300

    Added implementation for GiftCertificateRequestDto

--- src/main/java/com/vadim/springcore/dto/request/GiftCertificateRequestDto.java
+++ src/main/java/com/vadim/springcore/dto/request/GiftCertificateRequestDto.java
@@ -1,4 +1,32 @@
 package com.vadim.springcore.dto.request;
 
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@Builder
 public class GiftCertificateRequestDto {
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String description;
+
+    @NotNull
+    @Positive
+    private BigDecimal price;
+
+    @NotNull
+    private Integer duration;
+
+    private List<TagRequestDto> tags;
 }
