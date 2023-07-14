ef8b3b1332e449bef62e025ad9082da04b2cc2f7
VadimBelyaev02 <vadimbelaev002@gmail.com>
2023-07-14 14:59:48 +0300

    Added annotation for all args constructor

--- src/main/java/com/vadim/springcore/dto/request/TagRequestDto.java
+++ src/main/java/com/vadim/springcore/dto/request/TagRequestDto.java
@@ -1,5 +1,6 @@
 package com.vadim.springcore.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,6 +8,7 @@ import lombok.NoArgsConstructor;
 @Data
 @Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class TagRequestDto {
 
     private String name;
