530a8458342a42586d0ea4544e5397fe336919cf
VadimBelyaev02 <vadimbelaev002@gmail.com>
2023-07-14 17:41:32 +0300

    Added status field and static methods to reduce duplicated code

--- src/main/java/com/vadim/springcore/dto/response/ApiResponseDto.java
+++ src/main/java/com/vadim/springcore/dto/response/ApiResponseDto.java
@@ -1,21 +1,25 @@
 package com.vadim.springcore.dto.response;
 
-import lombok.Builder;
-import lombok.RequiredArgsConstructor;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.Instant;
 
 @Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ApiResponseDto<T> {
 
-    //@DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @JsonSerialize(using = ToStringSerializer.class)
     private Instant timestamp;
     private String message;
-
     private Color color;
-
     private T data;
+    private Integer status;
 
 
     @RequiredArgsConstructor
@@ -25,6 +29,22 @@ public class ApiResponseDto<T> {
         ERROR("red");
 
         private final String color;
+    }
 
+    public static <T> ApiResponseDto<T> successApiResponse(String message, T data) {
+        return ApiResponseDto.<T>builder()
+                .color(Color.SUCCESS)
+                .timestamp(Instant.now())
+                .message(message)
+                .data(data)
+                .build();
+    }
+
+    public static <T> ApiResponseDto<T> badApiResponse(String message) {
+        return ApiResponseDto.<T>builder()
+                .message(message)
+                .color(Color.ERROR)
+                .timestamp(Instant.now())
+                .build();
     }
 }
