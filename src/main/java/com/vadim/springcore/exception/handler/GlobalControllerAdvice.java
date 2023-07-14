3e64a55f31cba34aa7d26d1ca7cb127cfe977ea7
VadimBelyaev02 <vadimbelaev002@gmail.com>
2023-07-14 17:44:20 +0300

    Created exception handler

---
+++ src/main/java/com/vadim/springcore/exception/handler/GlobalControllerAdvice.java
@@ -0,0 +1,49 @@
+package com.vadim.springcore.exception.handler;
+
+import com.vadim.springcore.dto.response.ApiResponseDto;
+import com.vadim.springcore.exception.DuplicateRecordException;
+import com.vadim.springcore.exception.NotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import java.time.Instant;
+import java.util.Optional;
+
+@RestControllerAdvice
+public class GlobalControllerAdvice {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponseDto<?>> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        final ApiResponseDto<?> apiResponseDto = ApiResponseDto.builder()
+                .color(ApiResponseDto.Color.ERROR)
+                .message(exception.getMessage())
+                .timestamp(Instant.now())
+                .data(exception.getBody())
+                .build();
+
+        return new ResponseEntity<>(apiResponseDto, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ApiResponseDto<?>> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException exception) {
+        final ApiResponseDto<?> apiResponseDto = ApiResponseDto.badApiResponse(exception.getMessage());
+        return new ResponseEntity<>(apiResponseDto, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ApiResponseDto<?>> handleNotFoundException(NotFoundException exception) {
+
+        return null;
+    }
+
+    @ExceptionHandler({DuplicateRecordException.class})
+    public ResponseEntity<ApiResponseDto<?>> handleDuplicateRecordException(DuplicateRecordException exception) {
+
+        return null;
+    }
+}
