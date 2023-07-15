15f3eceede617663fa0a025382d3dd0f061b1c9c
VadimBelyaev02 <vadimbelaev002@gmail.com>
2023-07-15 16:39:39 +0300

    Created mappers to convert models from entity to dto and vice versa

---
+++ src/main/java/com/vadim/springcore/dto/mapper/GiftCertificateMapper.java
@@ -0,0 +1,26 @@
+package com.vadim.springcore.dto.mapper;
+
+import com.vadim.springcore.dto.request.GiftCertificateRequestDto;
+import com.vadim.springcore.dto.response.GiftCertificateResponseDto;
+import com.vadim.springcore.entity.GiftCertificate;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.MappingTarget;
+
+@Mapper(uses = TagMapper.class)
+public interface GiftCertificateMapper {
+
+
+  //  @Mapping(target = "lastUpdateDate", ignore = true)
+   // @Mapping(target = "createDate", ignore = true)
+    //@Mapping(target = "id", ignore = true)
+    GiftCertificate toEntity(GiftCertificateRequestDto dto);
+
+    GiftCertificateResponseDto toResponseDto(GiftCertificate giftCertificate);
+
+ //   @Mapping(target = "lastUpdateDate", ignore = true)
+  //  @Mapping(target = "id", ignore = true)
+   // @Mapping(target = "createDate", ignore = true)
+    void updateGiftCertificateFromDto(GiftCertificateRequestDto requestDto, @MappingTarget GiftCertificate giftCertificate);
+}
---
+++ src/main/java/com/vadim/springcore/dto/mapper/TagMapper.java
@@ -0,0 +1,19 @@
+package com.vadim.springcore.dto.mapper;
+
+import com.vadim.springcore.dto.request.TagRequestDto;
+import com.vadim.springcore.dto.response.TagResponseDto;
+import com.vadim.springcore.entity.Tag;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper
+public interface TagMapper {
+
+    Tag toEntity(TagRequestDto requestDto);
+
+    TagResponseDto toResponseDto(Tag tag);
+
+    List<Tag> tagRequestDtoListToTagList(List<TagRequestDto> tagRequestDtos);
+}
