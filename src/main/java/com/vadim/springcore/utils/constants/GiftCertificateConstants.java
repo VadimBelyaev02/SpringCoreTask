6819a0a4882e0a0e707f2108ccce18002a24af77
VadimBelyaev02 <vadimbelaev002@gmail.com>
2023-07-15 16:40:01 +0300

    Created utility class with GiftCertificate used constants

---
+++ src/main/java/com/vadim/springcore/utils/constants/GiftCertificateConstants.java
@@ -0,0 +1,12 @@
+package com.vadim.springcore.utils.constants;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class GiftCertificateConstants {
+
+    public final String GIFT_CERTIFICATE_NOT_FOUND_BY_ID = "Gift certificate with id = %s is not found";
+
+    public final String GIFT_CERTIFICATE_ALREADY_EXISTS_WITH_ID = "Gift certificate with id = %s already exists";
+
+}
