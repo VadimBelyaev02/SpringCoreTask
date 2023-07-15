75d6dc0f882a29a3b7be35037164ba42d6484c2f
VadimBelyaev02 <vadimbelaev002@gmail.com>
2023-07-15 17:09:16 +0300

    Refactoring

--- src/main/java/com/vadim/springcore/utils/constants/GiftCertificateConstants.java
+++ src/main/java/com/vadim/springcore/utils/constants/GiftCertificateConstants.java
@@ -5,7 +5,7 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class GiftCertificateConstants {
 
-    public final String GIFT_CERTIFICATE_NOT_FOUND_BY_ID = "Gift certificate with id = %s is not found";
+    public static final String GIFT_CERTIFICATE_NOT_FOUND_BY_ID = "Gift certificate with id = %s is not found";
 
     public final String GIFT_CERTIFICATE_ALREADY_EXISTS_WITH_ID = "Gift certificate with id = %s already exists";
 
