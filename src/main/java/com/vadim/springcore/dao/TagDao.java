a4f5ca72040c0cf3e768e46fbbfa36ec2ca7cc2e
VadimBelyaev02 <vadimbelaev002@gmail.com>
2023-07-16 12:18:10 +0300

    Created method for saving a batch of tags

--- src/main/java/com/vadim/springcore/dao/TagDao.java
+++ src/main/java/com/vadim/springcore/dao/TagDao.java
@@ -2,9 +2,12 @@ package com.vadim.springcore.dao;
 
 import com.vadim.springcore.entity.Tag;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface TagDao extends CrudDao<Tag, UUID> {
 
     boolean existsById(UUID id);
+
+    List<Tag> saveBatch(List<Tag> tags);
 }
