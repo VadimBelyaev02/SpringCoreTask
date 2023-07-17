5a014fbe35054a9ef4ccdceafac4f35dec3e5be7
VadimBelyaev02 <vadimbelaev002@gmail.com>
2023-07-17 17:21:06 +0300

    Wrote criteria search

--- src/main/java/com/vadim/springcore/service/impl/GiftCertificateServiceImpl.java
+++ src/main/java/com/vadim/springcore/service/impl/GiftCertificateServiceImpl.java
@@ -1,9 +1,12 @@
 package com.vadim.springcore.service.impl;
 
+import com.vadim.springcore.model.criteria.ComparatorUtil;
 import com.vadim.springcore.model.criteria.GiftCertificateCriteria;
 import com.vadim.springcore.dao.GiftCertificateDao;
 import com.vadim.springcore.dao.GiftCertificateTagDao;
 import com.vadim.springcore.dao.TagDao;
+import com.vadim.springcore.model.criteria.enums.SortField;
+import com.vadim.springcore.model.criteria.enums.SortType;
 import com.vadim.springcore.model.dto.mapper.GiftCertificateMapper;
 import com.vadim.springcore.model.dto.request.GiftCertificateRequestDto;
 import com.vadim.springcore.model.dto.response.GiftCertificateResponseDto;
@@ -15,12 +18,10 @@ import com.vadim.springcore.exception.NotFoundException;
 import com.vadim.springcore.service.GiftCertificateService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.UUID;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import static com.vadim.springcore.utils.constants.GiftCertificateConstants.GIFT_CERTIFICATE_NOT_FOUND_BY_ID;
@@ -50,12 +51,14 @@ public class GiftCertificateServiceImpl implements GiftCertificateService {
     }
 
     @Override
+    @Transactional
     public GiftCertificateResponseDto save(GiftCertificateRequestDto requestDto) {
         GiftCertificate giftCertificate = mapper.toEntity(requestDto);
         giftCertificate.setCreateDate(Instant.now());
         giftCertificate.setLastUpdateDate(Instant.now());
 
         final GiftCertificate savedGiftCertificate = giftCertificateDao.save(giftCertificate);
+        savedGiftCertificate.setTags(giftCertificate.getTags());
 
         saveTags(giftCertificate.getTags(), savedGiftCertificate.getId());
 //        giftCertificate.getTags().forEach(
@@ -64,8 +67,9 @@ public class GiftCertificateServiceImpl implements GiftCertificateService {
 //                    giftCertificateTagDao.save(new GiftCertificateTag(new GiftCertificateTagId(savedGiftCertificate.getId(), tag.getId())));
 //                }
 //        );
-        return mapper.toResponseDto(giftCertificateDao.save(giftCertificate));
+        return mapper.toResponseDto(savedGiftCertificate);
     }
+
 
     private void saveTags(List<Tag> tags, UUID giftCertificateId) {
         tags.forEach(
@@ -76,7 +80,10 @@ public class GiftCertificateServiceImpl implements GiftCertificateService {
         );
 
     }
+
     @Override
+    @Transactional
+
     public GiftCertificateResponseDto update(UUID id, GiftCertificateRequestDto requestDto) {
         GiftCertificate giftCertificate = giftCertificateDao.findById(id).orElseThrow(() ->
                 new NotFoundException(String.format(GIFT_CERTIFICATE_NOT_FOUND_BY_ID, id))
@@ -90,6 +97,7 @@ public class GiftCertificateServiceImpl implements GiftCertificateService {
     }
 
     @Override
+    @Transactional
     public void deleteById(UUID id) {
         if (!giftCertificateDao.existsById(id)) {
             throw new NotFoundException(String.format(GIFT_CERTIFICATE_NOT_FOUND_BY_ID, id));
@@ -104,9 +112,20 @@ public class GiftCertificateServiceImpl implements GiftCertificateService {
         if (Objects.nonNull(criteria.getTagName())) {
             lists.add(giftCertificateDao.findAllByTagName(criteria.getTagName()));
         }
-       // if (Objects.nonNull(criteria.getD))
+        if (Objects.nonNull(criteria.getPartOfTagName())) {
+            lists.add(giftCertificateDao.findAllLikeTagName(criteria.getPartOfTagName()));
+        }
+        if (Objects.nonNull(criteria.getPartOfDescription())) {
+            lists.add(giftCertificateDao.findAllLikeDescription(criteria.getPartOfDescription()));
+        }
 
-
-        return null;
+        return lists.stream()
+                .flatMap(List::stream).toList()
+                .stream()
+                .sorted(ComparatorUtil.getComparator(criteria.getFirstSortBy(), criteria.getFirstSortType())
+                        .thenComparing(ComparatorUtil.getComparator(criteria.getSecondSortBy(), criteria.getSecondSortType())))
+                .peek(gc -> gc.setTags(tagDao.findAllByGiftCertificateId(gc.getId())))
+                .map(mapper::toResponseDto)
+                .toList();
     }
 }
