749b240c6ddbc46984bf12c137b4c7c807fe8c14
VadimBelyaev02 <vadimbelaev002@gmail.com>
2023-07-17 10:45:28 +0300

    Created a new method

--- src/main/java/com/vadim/springcore/dao/GiftCertificateDao.java
+++ src/main/java/com/vadim/springcore/dao/GiftCertificateDao.java
@@ -2,9 +2,12 @@ package com.vadim.springcore.dao;
 
 import com.vadim.springcore.entity.GiftCertificate;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface GiftCertificateDao extends CrudDao<GiftCertificate, UUID> {
 
     boolean existsById(UUID id);
+
+    List<GiftCertificate> findAllByTagName(String tagName);
 }
