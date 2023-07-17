46659da74c2b3ecdc6b87d3ad49ee21c37e0845d
VadimBelyaev02 <vadimbelaev002@gmail.com>
2023-07-17 15:28:52 +0300

    Created method to fill entity fields from dto

--- src/main/java/com/vadim/springcore/model/dto/mapper/TagMapper.java
+++ src/main/java/com/vadim/springcore/model/dto/mapper/TagMapper.java
@@ -1,9 +1,12 @@
 package com.vadim.springcore.model.dto.mapper;
 
+import com.vadim.springcore.model.dto.request.GiftCertificateRequestDto;
 import com.vadim.springcore.model.dto.request.TagRequestDto;
 import com.vadim.springcore.model.dto.response.TagResponseDto;
+import com.vadim.springcore.model.entity.GiftCertificate;
 import com.vadim.springcore.model.entity.Tag;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 import java.util.List;
 
@@ -15,4 +18,7 @@ public interface TagMapper {
     TagResponseDto toResponseDto(Tag tag);
 
     List<Tag> tagRequestDtoListToTagList(List<TagRequestDto> tagRequestDtos);
+
+    void updateTagFromDto(TagRequestDto requestDto, @MappingTarget Tag tag);
+
 }
