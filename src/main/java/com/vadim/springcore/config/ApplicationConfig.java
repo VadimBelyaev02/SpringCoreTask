bdfabc441c507ff6c1e9f6fd7cd1014f64030b61
VadimBelyaev02 <vadimbelaev002@gmail.com>
2023-07-19 20:02:37 +0300

    Some fixes

--- src/main/java/com/vadim/springcore/config/ApplicationConfig.java
+++ src/main/java/com/vadim/springcore/config/ApplicationConfig.java
@@ -1,10 +1,9 @@
 package com.vadim.springcore.config;
 
 import com.zaxxer.hikari.HikariConfig;
-import jakarta.servlet.ServletContext;
-import jakarta.servlet.ServletException;
 import liquibase.integration.spring.SpringLiquibase;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.*;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -12,10 +11,10 @@ import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import javax.sql.DataSource;
+
 
 @EnableWebMvc
 @EnableTransactionManagement
@@ -23,33 +22,32 @@ import javax.sql.DataSource;
 @ComponentScan(basePackages = "com.vadim.springcore")
 @RequiredArgsConstructor
 @PropertySource("classpath:properties/application.properties")
-public class ApplicationConfig implements WebApplicationInitializer {
+public class ApplicationConfig {
 
     private final Environment environment;
 
+//    @Bean
+//    @Primary
+//    public DataSource dataSource() {
+//        HikariConfig hikariConfig = new HikariConfig();
+//        hikariConfig.setDriverClassName(environment.getRequiredProperty("db.driverClassName"));
+//        hikariConfig.setJdbcUrl(environment.getRequiredProperty("db.url"));
+//        hikariConfig.setPassword(environment.getRequiredProperty("db.password"));
+//        hikariConfig.setUsername(environment.getRequiredProperty("db.username"));
+//       // hikariConfig.addDataSourceProperty("cachePrepStmts", "true");
+//        //   hikariConfig.addDataSourceProperty("prepStmtCacheSqlLimit", "512");
+//        return hikariConfig.getDataSource();
+//    }
 
     @Bean
-    @Profile("dev")
     public DataSource dataSource() {
-        HikariConfig hikariConfig = new HikariConfig();
-        hikariConfig.setJdbcUrl(environment.getRequiredProperty("db.url"));
-        hikariConfig.setPassword(environment.getRequiredProperty("db.password"));
-        hikariConfig.setUsername(environment.getRequiredProperty("db.username"));
-        hikariConfig.addDataSourceProperty("cachePrepStmts", "true");
-        hikariConfig.addDataSourceProperty("prepStmtCacheSqlLimit", "512");
-        return hikariConfig.getDataSource();
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUsername(environment.getRequiredProperty("db.username"));
+        dataSource.setPassword(environment.getRequiredProperty("db.password"));
+        dataSource.setDriverClassName(environment.getRequiredProperty("db.driverClassName"));
+        dataSource.setUrl(environment.getRequiredProperty("db.url"));
+        return dataSource;
     }
-
-//    @Bean
-//    @Profile("dev")
-//    public DataSource dataSource() {
-//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//        dataSource.setUsername(environment.getRequiredProperty("db.username"));
-//        dataSource.setPassword(environment.getRequiredProperty("db.password"));
-//        dataSource.setDriverClassName(environment.getRequiredProperty("db.driverClassName"));
-//        dataSource.setUrl(environment.getRequiredProperty("db.url"));
-//        return dataSource;
-//    }
 
     @Bean
     public JdbcTemplate jdbcTemplate(DataSource dataSource) {
@@ -69,10 +67,5 @@ public class ApplicationConfig implements WebApplicationInitializer {
         DataSourceTransactionManager transactionManager = new DataSourceTransactionManager();
         transactionManager.setDataSource(dataSource());
         return transactionManager;
-    }
-
-    @Override
-    public void onStartup(ServletContext servletContext) throws ServletException {
-        servletContext.setInitParameter("spring.profile.active", "dev");
     }
 }
