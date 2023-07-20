ce39bd389395c2ab7b9cedb1791b1d28a8ceca61
VadimBelyaev02 <vadimbelaev002@gmail.com>
2023-07-20 19:00:15 +0300

    Created interface for factory classes for dto and entity classes

---
+++ src/test/java/com/vadim/springcore/factory/ModelFactory.java
@@ -0,0 +1,25 @@
+package com.vadim.springcore.factory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface ModelFactory<T> {
+
+    Integer defaultListSize = 3;
+    T getInstance();
+
+    default List<T> getInstanceList() {
+        return getInstanceList(3);
+    }
+
+    default List<T> getInstanceList(Integer size) {
+        size = Math.max(1, size);
+
+        List<T> list = new ArrayList<>(size);
+
+        while (size-- > 0) {
+            list.add(getInstance());
+        }
+        return list;
+    }
+}
