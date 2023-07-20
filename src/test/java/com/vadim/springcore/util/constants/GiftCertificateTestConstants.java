820e7cbd133fb30eb720422751a3da70277ee2da
VadimBelyaev02 <vadimbelaev002@gmail.com>
2023-07-20 19:14:22 +0300

    Classes with constants

---
+++ src/test/java/com/vadim/springcore/util/constants/GiftCertificateTestConstants.java
@@ -0,0 +1,19 @@
+package com.vadim.springcore.util.constants;
+
+import lombok.experimental.UtilityClass;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.UUID;
+
+@UtilityClass
+public class GiftCertificateTestConstants {
+
+    public static final UUID ID = UUID.fromString("ea1d28e4-2c1e-4815-ab11-6a02792d34b0");
+    public static final String NAME = "name";
+    public static final BigDecimal PRICE = BigDecimal.TEN;
+    public static final String DESCRIPTION = "description";
+    public static final Integer DURATION = 100;
+    public static final Instant CREATE_DATE = Instant.MIN;
+    public static final Instant LAST_UPDATE_DATE = Instant.MAX;
+}
---
+++ src/test/java/com/vadim/springcore/util/constants/TagTestConstants.java
@@ -0,0 +1,14 @@
+package com.vadim.springcore.util.constants;
+
+import lombok.experimental.UtilityClass;
+
+import java.util.UUID;
+
+@UtilityClass
+public class TagTestConstants {
+
+    public static final String NAME = "name";
+    public static final UUID ID = UUID.fromString("250ef41b-de69-4973-987d-3e36e1152595");
+
+
+}
