c57f6f5e7e746a51b3193a4d08a0c2362775ce73
VadimBelyaev02 <vadimbelaev002@gmail.com>
2023-07-21 13:46:52 +0300

    Fixed ApiResponseDto and exception handler

--- src/main/java/com/vadim/springcore/exception/handler/GlobalControllerAdvice.java
+++ src/main/java/com/vadim/springcore/exception/handler/GlobalControllerAdvice.java
@@ -10,37 +10,39 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
-import java.time.Instant;
-
 @RestControllerAdvice
 public class GlobalControllerAdvice {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ApiResponseDto<?>> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
-        final ApiResponseDto<?> apiResponseDto = ApiResponseDto.builder()
-                .color(ApiResponseDto.Color.ERROR)
-                .message(exception.getMessage())
-                .timestamp(Instant.now())
-                .data(exception.getBody())
-                .build();
-
+        final ApiResponseDto<?> apiResponseDto = ApiResponseDto.badApiResponse(
+                exception.getMessage(),
+                exception.getBody()
+        );
         return new ResponseEntity<>(apiResponseDto, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<ApiResponseDto<?>> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException exception) {
-        final ApiResponseDto<?> apiResponseDto = ApiResponseDto.badApiResponse(exception.getMessage());
+        final ApiResponseDto<?> apiResponseDto = ApiResponseDto.badApiResponse(
+                exception.getMessage()
+        );
         return new ResponseEntity<>(apiResponseDto, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ApiResponseDto<?>> handleNotFoundException(NotFoundException exception) {
-
-        return null;
+        final ApiResponseDto<?> apiResponseDto = ApiResponseDto.badApiResponse(
+                exception.getMessage()
+        );
+        return new ResponseEntity<>(apiResponseDto, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler({DuplicateRecordException.class})
     public ResponseEntity<ApiResponseDto<?>> handleDuplicateRecordException(DuplicateRecordException exception) {
-        return null;
+        final ApiResponseDto<?> apiResponseDto = ApiResponseDto.badApiResponse(
+                exception.getMessage()
+        );
+        return new ResponseEntity<>(apiResponseDto, HttpStatus.CONFLICT);
     }
 }
