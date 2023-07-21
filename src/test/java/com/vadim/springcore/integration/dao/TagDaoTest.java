8f740fd7a8c1786535487a5007c21a2694cb0f42
VadimBelyaev02 <vadimbelaev002@gmail.com>
2023-07-21 12:18:15 +0300

    Finished tests + added test data

--- src/test/java/com/vadim/springcore/integration/dao/TagDaoTest.java
+++ src/test/java/com/vadim/springcore/integration/dao/TagDaoTest.java
@@ -10,54 +10,147 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
-//@Transactional
+import static com.vadim.springcore.util.constants.TagTestConstants.ID;
+import static org.junit.jupiter.api.Assertions.*;
+
+
 // active profile
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = {TestConfig.class})
 @WebAppConfiguration
-
-//@Rollback
+@Transactional
 public class TagDaoTest {
 
     @Autowired
     private TagDao dao;
 
-    /*
-        boolean existsById(UUID id);
-
-        Tag saveIfNotExists(Tag tag);
-
-        List<Tag> findAllByGiftCertificateId(UUID id);
-
-        boolean existsByName(String name);
-
-        List<T> findAll();
-
-        Optional<T> findById(K id);
-
-       1 T save(T t);
-
-        T update(T t);
-
-        void deleteById(K id);
-
-     */
+    private final int numberOfTags = 4;
 
     @Test
-    void save() {
+    void saveTestWithNotExistingTag() {
         Tag tag = TagFactory.getTagFactory().getInstance();
-        tag.setId(null);
         Tag savedTag = dao.save(tag);
-
         tag.setId(savedTag.getId());
 
         assertEquals(tag, savedTag);
+        assertEquals(numberOfTags + 1, dao.findAll().size());
     }
-    @Test
-    void existsById() {
 
+    @Test
+    void saveIfNotExistsTestWithNotExistingTagByName() {
+        Tag tag = TagFactory.getTagFactory().getInstance();
+        tag.setName("a new name");
+
+        Tag savedTag = dao.saveIfNotExists(tag);
+        tag.setId(savedTag.getId());
+
+        assertEquals(tag, savedTag);
+        assertEquals(numberOfTags + 1, dao.findAll().size());
+    }
+
+    @Test
+    void saveIfNotExistsTestWithExistingTagByName() {
+        Tag tag = TagFactory.getTagFactory().getInstance();
+        tag.setName("name1");
+
+        Tag savedTag = dao.saveIfNotExists(tag);
+        tag.setId(savedTag.getId());
+
+        assertEquals(tag, savedTag);
+        assertEquals(numberOfTags, dao.findAll().size());
+    }
+
+    @Test
+    void findAllByGiftCertificateIdTest() {
+        UUID giftCertificateID = UUID.fromString("7120a2ad-215a-4c3b-9f5e-f80234c15eba");
+
+        List<Tag> tags = dao.findAllByGiftCertificateId(giftCertificateID);
+
+        assertEquals(2, tags.size());
+    }
+
+    @Test
+    void existsByIdTestWithExistingId() {
+        Tag tag = dao.findAll().get(0);
+
+        assertTrue(dao.existsById(ID));
+    }
+
+    @Test
+    void existsByIdTestWithNotExistingId() {
+        UUID id = UUID.randomUUID();
+
+        assertFalse(dao.existsById(id));
+    }
+
+    @Test
+    void existsByNameTestWithNotExistingName() {
+        String name = "random name";
+
+        assertFalse(dao.existsByName(name));
+    }
+
+    @Test
+    void existsByNameTestWithExistingName() {
+        String name = dao.findAll().get(0).getName();
+
+        assertTrue(dao.existsByName(name));
+    }
+
+    @Test
+    void findAllTest() {
+        assertEquals(numberOfTags, dao.findAll().size());
+    }
+
+    @Test
+    void findByIdTestWithExistingId() {
+        Tag tag = dao.findAll().get(0);
+        UUID id = tag.getId();
+
+        Optional<Tag> optionalTag = dao.findById(id);
+
+        assertTrue(optionalTag.isPresent());
+        assertEquals(tag, optionalTag.get());
+    }
+
+    @Test
+    void findByIdTestWithNotExistingId() {
+        UUID id = UUID.randomUUID();
+
+        Optional<Tag> optionalTag = dao.findById(id);
+
+        assertFalse(optionalTag.isPresent());
+    }
+
+    @Test
+    void deleteById() {
+        UUID id = UUID.fromString("78512969-138e-4a2c-96ff-34b0e302ed08");
+
+        dao.deleteById(id);
+
+        Optional<Tag> optionalTag = dao.findById(id);
+
+        assertFalse(optionalTag.isPresent());
+        assertEquals(numberOfTags - 1, dao.findAll().size());
+    }
+
+    @Test
+    void updateTest() {
+        Tag tag = dao.findAll().get(0);
+        UUID id = tag.getId();
+        tag.setName("a new name");
+
+        dao.update(tag);
+
+        Optional<Tag> optionalTag = dao.findById(id);
+
+        assertTrue(optionalTag.isPresent());
+        assertEquals(tag, optionalTag.get());
     }
 }
