1d02c8554c9e41419eca2b83a06842de1d72e9c8
VadimBelyaev02 <vadimbelaev002@gmail.com>
2023-07-24 10:39:52 +0300

    Versions now are in properties + added profile for testing

--- src/test/java/com/vadim/springcore/config/TestConfig.java
+++ src/test/java/com/vadim/springcore/config/TestConfig.java
@@ -4,6 +4,7 @@ import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
@@ -17,7 +18,7 @@ import javax.sql.DataSource;
 @ComponentScan(basePackages = "com.vadim.springcore")
 @EnableWebMvc
 @WebAppConfiguration
-//@Profile("test")
+@Profile("test")
 public class TestConfig {
 
     @Bean
