33e1b26d085d64771ae98ab97711659dbf0ae505
VadimBelyaev02 <vadimbelaev002@gmail.com>
2023-07-25 18:08:03 +0300

    Added new fields

--- src/main/java/com/vadim/springtask/model/entity/GiftCertificate.java
+++ src/main/java/com/vadim/springtask/model/entity/GiftCertificate.java
@@ -1,7 +1,14 @@
 package com.vadim.springtask.model.entity;
 
+import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.UuidGenerator;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.time.Instant;
@@ -10,21 +17,44 @@ import java.util.UUID;
 
 @Getter
 @Setter
-@EqualsAndHashCode
 @Builder
-@ToString
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "gift_certificates")
 public class GiftCertificate {
 
+    @Id
+    @GeneratedValue
+    @UuidGenerator
     private UUID id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "price")
     private BigDecimal price;
+
+    @Column(name = "duration")
     private Integer duration;
+
+    @CreatedDate
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     private Instant createDate;
+
+    @LastModifiedDate
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     private Instant lastUpdateDate;
+
+    @Column(name = "description")
     private String description;
+
+    @ManyToMany(cascade = {CascadeType.MERGE, CascadeType.PERSIST}, fetch = FetchType.EAGER)
+    @JoinTable(name = "gift_certificates_tags",
+            joinColumns = @JoinColumn(name = "gift_certificate_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "tag_id", referencedColumnName = "id"))
     private List<Tag> tags;
+
+    @OneToMany(mappedBy = "giftCertificate", cascade =  {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST}, fetch = FetchType.LAZY)
+    private List<Order> orders;
 }
