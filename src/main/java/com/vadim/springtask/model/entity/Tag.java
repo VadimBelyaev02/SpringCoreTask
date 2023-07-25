fabb2ef701808147eeae52b287bf63a49f93877e
VadimBelyaev02 <vadimbelaev002@gmail.com>
2023-07-25 18:08:34 +0300

    Added fields

--- src/main/java/com/vadim/springtask/model/entity/Tag.java
+++ src/main/java/com/vadim/springtask/model/entity/Tag.java
@@ -1,18 +1,34 @@
 package com.vadim.springtask.model.entity;
 
 
+import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.UuidGenerator;
 
+import java.util.List;
 import java.util.UUID;
 
 @Getter
 @Setter
-@EqualsAndHashCode
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "tags")
 public class Tag {
 
+    @Id
+    @GeneratedValue
+    @UuidGenerator
     private UUID id;
+
+    @Column(name = "name")
     private String name;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "gift_certificates_tags",
+            joinColumns = @JoinColumn(name = "tag_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "gift_certificate_id", referencedColumnName = "id"))
+    private List<GiftCertificate> giftCertificates;
+
 }
