308a6e5263ecfad7f7fa3df0b1b8d8e02be89c3b
VadimBelyaev02 <vadimbelaev002@gmail.com>
2023-07-25 18:09:07 +0300

    SpringTaskApplication

--- src/main/java/com/vadim/springtask/SpringTaskApplication.java
+++ src/main/java/com/vadim/springtask/SpringTaskApplication.java
@@ -1,4 +1,12 @@
 package com.vadim.springtask;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class SpringTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTaskApplication.class, args);
+    }
+
 }
