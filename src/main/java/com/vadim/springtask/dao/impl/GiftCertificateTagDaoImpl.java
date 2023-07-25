1682cc0ee9931e8dce02c76580733739f5fff62b
VadimBelyaev02 <vadimbelaev002@gmail.com>
2023-07-25 18:11:03 +0300

    Rewritten using entity manager

--- src/main/java/com/vadim/springtask/dao/impl/GiftCertificateTagDaoImpl.java
+++ src/main/java/com/vadim/springtask/dao/impl/GiftCertificateTagDaoImpl.java
@@ -1,11 +1,15 @@
 package com.vadim.springtask.dao.impl;
 
 import com.vadim.springtask.dao.GiftCertificateTagDao;
+import com.vadim.springtask.model.entity.GiftCertificate;
 import com.vadim.springtask.model.entity.GiftCertificateTag;
 import com.vadim.springtask.model.entity.GiftCertificateTagId;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,76 +17,64 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
 
-@Repository
+@Component
 @RequiredArgsConstructor
 public class GiftCertificateTagDaoImpl implements GiftCertificateTagDao {
 
-    private final JdbcTemplate template;
-
-    private final RowMapper<GiftCertificateTag> mapper = (rs, rowNum) -> {
-        final UUID giftCertificate_Id = UUID.fromString(rs.getString("gift_certificate_id"));
-        final UUID tagId = UUID.fromString(rs.getString("tag_id"));
-
-        return GiftCertificateTag.builder()
-                .id(new GiftCertificateTagId(giftCertificate_Id, tagId))
-                .build();
-    };
-
+    @PersistenceContext
+    private final EntityManager manager;
     @Override
     public List<GiftCertificateTag> findAll() {
-        final String SQL = "SELECT * FROM gift_certificates_tags";
-        return template.query(SQL, mapper);
+        String query = "SELECT s FROM GiftCertificateTag s";
+        return manager.createQuery(query, GiftCertificateTag.class).getResultList();
     }
 
     @Override
     public Optional<GiftCertificateTag> findById(GiftCertificateTagId id) {
-        final String SQL = "SELECT * FROM gift_certificates_tags WHERE gift_certificate_id = ? AND tag_id = ?";
-        return template.query(SQL, mapper, id.getGiftCertificateId(), id.getTagId()).stream()
-                .findFirst();
+        GiftCertificateTag giftCertificateTag = manager.find(GiftCertificateTag.class, id);
+        if (Objects.nonNull(giftCertificateTag)) {
+            manager.detach(giftCertificateTag);
+        }
+        return Optional.ofNullable(giftCertificateTag);
     }
 
     @Override
     public GiftCertificateTag save(GiftCertificateTag giftCertificateTag) {
-        if (Objects.nonNull(giftCertificateTag.getId()) && existsById(giftCertificateTag.getId())) {
-            update(giftCertificateTag);
-        }
-
-        final String SQL = "INSERT INTO gift_certificates_tags (gift_certificate_id, tag_id) VALUES (?, ?)";
-        template.update(SQL, giftCertificateTag.getId().getGiftCertificateId(), giftCertificateTag.getId().getTagId());
-
+        manager.persist(giftCertificateTag);
         return giftCertificateTag;
     }
 
     @Override
     public GiftCertificateTag update(GiftCertificateTag giftCertificateTag) {
-        final String SQL = "UPDATE gift_certificates_tags SET gift_certificate_id = ?, tag_id = ? WHERE gift_certificate_id = ? AND tag_id = ?";
-        UUID giftCertificateId = giftCertificateTag.getId().getGiftCertificateId();
-        UUID tagId = giftCertificateTag.getId().getTagId();
-        template.update(SQL, giftCertificateId, tagId, giftCertificateId, tagId);
-        return giftCertificateTag;
+        return manager.merge(giftCertificateTag);
     }
 
     @Override
     public void deleteById(GiftCertificateTagId id) {
-        final String SQL = "DELETE FROM gift_certificates_tags WHERE gift_certificate_id = ? AND tag_id = ?";
-        template.update(SQL, id.getGiftCertificateId(), id.getGiftCertificateId());
+        String query = "DELETE FROM GiftCertificateTag gc WHERE gc.id = :id";
+        manager.createQuery(query)
+                .setParameter("id", id)
+                .executeUpdate();
     }
 
     @Override
     public void deleteByGiftCertificateId(UUID id) {
-        final String SQL = "DELETE FROM gift_certificates_tags WHERE gift_certificate_id = ?";
-        template.update(SQL, id);
+        final String query = "DELETE FROM GiftCertificateTag gct WHERE gct.id.giftCertificate.id = :gcId";
+        manager.createQuery(query)
+                .setParameter("gcId", id)
+                .executeUpdate();
     }
 
     @Override
     public void deleteByTagId(UUID id) {
-        final String SQL = "DELETE FROM gift_certificates_tags WHERE tag_id = ?";
-        template.update(SQL, id);
+        final String query = "DELETE FROM GiftCertificateTag gct WHERE gct.id = :tagId";
+        manager.createQuery(query)
+                .setParameter("tagId", id)
+                .executeUpdate();
     }
 
     @Override
     public boolean existsById(GiftCertificateTagId id) {
-        final String SQL = "SELECT * FROM gift_certificates_tags WHERE gift_certificate_id = ? AND tag_id = ?";
-        return template.query(SQL, mapper, id.getGiftCertificateId(), id.getTagId()).size() > 0;
+        return Objects.nonNull(manager.find(GiftCertificateTag.class, id));
     }
 }
