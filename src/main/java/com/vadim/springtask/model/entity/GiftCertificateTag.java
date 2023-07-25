22307d29e0b6872d5f747e85c8479af28e99827b
VadimBelyaev02 <vadimbelaev002@gmail.com>
2023-07-25 18:12:19 +0300

    Added jpa annotations

--- src/main/java/com/vadim/springtask/model/entity/GiftCertificateTag.java
+++ src/main/java/com/vadim/springtask/model/entity/GiftCertificateTag.java
@@ -1,5 +1,8 @@
 package com.vadim.springtask.model.entity;
 
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.*;
 
 @Getter
@@ -7,7 +10,11 @@ import lombok.*;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "gift_certificates_tags")
 public class GiftCertificateTag {
 
+    @EmbeddedId
     private GiftCertificateTagId id;
+
 }
--- src/main/java/com/vadim/springtask/model/entity/GiftCertificateTagId.java
+++ src/main/java/com/vadim/springtask/model/entity/GiftCertificateTagId.java
@@ -1,6 +1,8 @@
 package com.vadim.springtask.model.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,6 +16,9 @@ import java.util.UUID;
 @Data
 public class GiftCertificateTagId implements Serializable {
 
+    @Column(name = "gift_certificate_id")
     private UUID giftCertificateId;
+
+    @Column(name = "tag_id")
     private UUID tagId;
 }
