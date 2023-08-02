503279734f1810a50442dbf9c28d9d9cb9c4ff61
VadimBelyaev02 <vadimbelaev002@gmail.com>
2023-08-02 16:11:15 +0300

    Small fixes

--- src/main/java/com/vadim/springtask/service/impl/UserServiceImpl.java
+++ src/main/java/com/vadim/springtask/service/impl/UserServiceImpl.java
@@ -3,11 +3,13 @@ package com.vadim.springtask.service.impl;
 import com.vadim.springtask.dao.UserDao;
 import com.vadim.springtask.exception.NotFoundException;
 import com.vadim.springtask.model.dto.mapper.UserMapper;
+import com.vadim.springtask.model.dto.response.PageResponseDto;
 import com.vadim.springtask.model.dto.response.UserResponseDto;
 import com.vadim.springtask.model.entity.User;
 import com.vadim.springtask.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
@@ -22,6 +24,7 @@ public class UserServiceImpl implements UserService {
     private final UserMapper mapper;
 
     @Override
+    @Transactional(readOnly = true)
     public UserResponseDto getById(UUID id) {
         User user = dao.findById(id).orElseThrow(() ->
                new NotFoundException(String.format(USER_NOT_FOUND_BY_ID, id))
@@ -30,9 +33,17 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public List<UserResponseDto> getAll() {
-        return dao.findAll().stream()
+    @Transactional(readOnly = true)
+    public PageResponseDto<UserResponseDto> getAll(Integer page, Integer pageSize) {
+        List<UserResponseDto> userResponseDtos = dao.findAll(page, pageSize).stream()
                 .map(mapper::toResponseDto)
                 .toList();
+
+        return PageResponseDto.<UserResponseDto>builder()
+                .size(pageSize)
+                .elementsAmount(userResponseDtos.size())
+                .pageNumber(page)
+                .content(userResponseDtos)
+                .build();
     }
 }
